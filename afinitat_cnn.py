f19efc39138cb87037c6d5d4efa5e1062a55fc8f
rvg22 <rvg5423@outlook.com>
2022-12-07 20:30:37 +0100

    Added convertir()

--- afinitat_cnn.py
+++ afinitat_cnn.py
@@ -1,7 +1,6 @@
 from arreglar_dades import neteja_dades_afinitat
 import pandas as pd
 from tensorflow import keras
-import re
 
 dades = neteja_dades_afinitat()
 
@@ -14,17 +13,47 @@ elements_smiles = ['6', '3', '=', 'H', 'C', 'O', 'c', '#', 'a', '[', 't', 'r', '
                    '0', 'L', '%', 'g', '9', 'Z', '(', 'N', '8', 'I', '7', '5', 'l', ')', 'A', 'e', 'o', 'V', 's', 'S', '2', 'M', 'T', 'u', 'i']
 # elements_smiles fa referencia a els elements pels quals es poden formar els smiles
 
-int_smiles = dict(zip(elements_smiles, range(1, len(elements_smiles))))
+int_smiles = dict(zip(elements_smiles, range(1, len(elements_smiles)+1)))
+# Per associar tots els elements amb un int determinat(range és 1, len+1 perquè es plenen amb zeros per arribar a maxim_smiles)
 
 maxim_fasta = 5000
 elements_fasta = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
-                  'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+                  'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']  # Format pels diferents aa que formen els fasta
 
 int_fasta = dict(zip(elements_fasta, range(1, len(elements_fasta))))
+# Per associar tots els elements amb un int determinat(range és 1, len+1 perquè es plenen amb zeros per arribar a maxim_fasta)
+
+
+def convertir(arx):
+    '''
+        Funció per convertir tots els elements (tant smiles, com fasta) en int, per tal de ser entrenats al model
+
+    '''
+
+    smiles_amb_numeros = []  # Smiles obtinguts amb int_smiles[1] i els smiles del df
+    for i in arx.smiles:
+        for elements in i:  # Elements fa referència a els elements que formen elements_smile
+            smiles_amb_numeros.append(int_smiles[elements])
+        while len(i) < maxim_smiles:
+            smiles_amb_numeros.append(0)
+
+    fasta_amb_numeros = []
+    for i in arx.sequence:
+        for elements in i:  # Elements fa referència a els elements que formen elements_smile
+            fasta_amb_numeros.append(int_fasta[elements])
+        while len(i) < maxim_smiles:
+            fasta_amb_numeros.append(0)
+
+    ic50_numeros = list(arx.IC50)
+
+    return smiles_amb_numeros, fasta_amb_numeros, ic50_numeros
 
 
 def model_cnn():
+    ''' 
+        Model per entrenar les dades.
 
+    '''
     # model per smiles
     i = keras.keras.Input(shape=(maxim_smiles,))
     embed = keras.keras.layers.Embedding(input_dim=len(
