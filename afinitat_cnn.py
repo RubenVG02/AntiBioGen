ca499ffbf6e6c334304a2ec35bb17996a6d8b720
rvg22 <rvg5423@outlook.com>
2022-12-07 20:03:15 +0100

    modified:   afinitat_cnn.py

--- afinitat_cnn.py
+++ afinitat_cnn.py
@@ -6,31 +6,21 @@ import re
 dades = neteja_dades_afinitat()
 
 arx = pd.read_csv(dades, sep=",")
-re.sub("nan", "", arx["IC50"])
-datfr = arx[arx.IC50.notna()]
-datfr = datfr[arx.smiles.notna()]
-# print(datfr)
-df_no_dup = datfr.drop_duplicates(['smiles'])
-df_no_dup["IC50"].astype(float)
-re.sub(">", "", df_no_dup["IC50"])
-df2 = df_no_dup[df_no_dup["IC50"] <= 1000000]
 
-df2 = df2[df2["IC50"] > 0]
-df2 = df2.reset_index(drop=True)
-df2.to_csv(f"prueba.csv", index=False, sep=";")
 
-print(arx.head())
-print(len(arx))
-print(len(arx["IC50"] > 1000000))
 # valor maxim que vull que tingin les meves smiles, serviran per entrenar el model
 maxim_smiles = 140
 elements_smiles = ['6', '3', '=', 'H', 'C', 'O', 'c', '#', 'a', '[', 't', 'r', 'K', 'n', 'B', 'F', '4', '+', ']', '-', '1', 'P',
                    '0', 'L', '%', 'g', '9', 'Z', '(', 'N', '8', 'I', '7', '5', 'l', ')', 'A', 'e', 'o', 'V', 's', 'S', '2', 'M', 'T', 'u', 'i']
 # elements_smiles fa referencia a els elements pels quals es poden formar els smiles
 
+int_smiles = dict(zip(elements_smiles, range(1, len(elements_smiles))))
+
 maxim_fasta = 5000
 elements_fasta = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
                   'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+
+int_fasta = dict(zip(elements_fasta, range(1, len(elements_fasta))))
 
 
 def model_cnn():
@@ -63,7 +53,7 @@ def model_cnn():
     x2 = keras.keras.layers.Conv1D(
         filters=128, kernel_size=3, padding="SAME")(x2)
     x2 = keras.keras.layers.PReLU()(x2)
-    pool2 = keras.keras.layers.GlobalAvgPool1D()(
+    pool2 = keras.keras.layers.GlobalMaxPooling1D()(
         x2)  # maxpool per obtenir un vector de 1d
 
     junt = keras.keras.layers.concatenate(pool, pool2)
@@ -80,6 +70,7 @@ def model_cnn():
 
     modelo = keras.keras.models.Model(inputs=[i, i2], output=op)
 
-    modelo.compile(optimizer="adam", loss="mse")
+    modelo.compile(optimizer="adam", loss="mse",
+                   metrics=keras.keras.metrics.MeanSquaredError())
 
     modelo.fit()
