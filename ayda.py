999ceb684bee99456b0010bf08758d0854683786
rvg22 <rvg5423@outlook.com>
2022-12-07 17:55:56 +0100

    modified:   .gitignore 	modified:   arreglar_dades.py 	modified:   ayda.py

--- ayda.py
+++ ayda.py
@@ -5,16 +5,17 @@ import re
 
 dades = neteja_dades_afinitat()
 
-arx = pd.read_csv(dades, sep=",")
-arx["IC50"].str.strip()
-arx = arx.dropna(how="any").reset_index(drop=True)
-# print(datfr)
-df_no_dup = arx.drop_duplicates(['smiles'])
-df_no_dup["IC50"] = df_no_dup["IC50"].str.replace(r"[<>]", "", regex=True)
-df_no_dup["IC50"] = df_no_dup["IC50"].astype(float)
-#df_no_dup["IC50"] = df_no_dup[df_no_dup["IC50"] < 1000000]
-print("Hasta aqui va bien")
-df_no_dup.to_csv(f"prueba.csv", index=False, sep=";")
 
-print(arx.head())
-print(len(arx))
+def neteka_dades_cnn(arx_final="CNN_Net"):
+    arx = pd.read_csv(dades, sep=",")
+    arx["IC50"].str.strip()
+    arx = arx.dropna(how="any").reset_index(drop=True)
+    # print(datfr)
+    df_no_dup = arx.drop_duplicates(['smiles'])
+    df_no_dup["IC50"] = df_no_dup["IC50"].str.replace(r"[<>]", "", regex=True)
+    df_no_dup["IC50"] = df_no_dup["IC50"].astype(float)
+    #df_no_dup["IC50"] = df_no_dup[df_no_dup["IC50"] < 1000000]
+    df_no_dup.to_csv(f"{arx_final}.csv", index=False, sep=";")
+
+    print(arx.head())
+    print(len(arx))
