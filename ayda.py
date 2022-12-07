ab1709dcf77459e8dced954d9add38dce0689788
rvg22 <rvg5423@outlook.com>
2022-12-07 17:49:06 +0100

    Fixed bug with IC50 values

--- ayda.py
+++ ayda.py
@@ -7,18 +7,14 @@ dades = neteja_dades_afinitat()
 
 arx = pd.read_csv(dades, sep=",")
 arx["IC50"].str.strip()
-arx[arx["IC50"].str.contains("nan") == False]
-datfr = arx[arx.smiles.notna()]
+arx = arx.dropna(how="any").reset_index(drop=True)
 # print(datfr)
-df_no_dup = datfr.drop_duplicates(['smiles'])
+df_no_dup = arx.drop_duplicates(['smiles'])
 df_no_dup["IC50"] = df_no_dup["IC50"].str.replace(r"[<>]", "", regex=True)
 df_no_dup["IC50"] = df_no_dup["IC50"].astype(float)
-'''df2 = df_no_dup[df_no_dup["IC50"] <= 1000000]'''
+#df_no_dup["IC50"] = df_no_dup[df_no_dup["IC50"] < 1000000]
 print("Hasta aqui va bien")
-'''df2 = df2[df2["IC50"] > 0]
-df2 = df2.reset_index(drop=True)'''
 df_no_dup.to_csv(f"prueba.csv", index=False, sep=";")
 
 print(arx.head())
 print(len(arx))
-print(len(arx["IC50"] > 1000000))
