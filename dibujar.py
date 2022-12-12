5840e4b7109a4baa81608ea54678d7c3954a9867
rvg22 <rvg5423@outlook.com>
2022-12-12 01:02:33 +0100

    dibujar

--- dibujar.py
+++ dibujar.py
@@ -4,7 +4,10 @@ from rdkit.Chem import Draw
 from IPython.display import SVG
 
 
-smile = "COCCOc1cc2ncc(C#N)c(Nc3cc(OC)c(OC)c(OC)c3)c2cc1OCCOC"
+smile = '''c(cc12)C1=CCNCC1
+O=C1N[CH](CN2C(=O)C(=OC(=O)C(=OC(=O)C2=O)c1cccc(c1)C#N)C1CC1
+OC(=O)c1ccc(cc1)S(=O)(=O)c1ccc(Oc2ccc(F)cc2F)c(=O)c1C
+Cc1cc'''
 
 molecula = Chem.MolFromSmiles(smile)
 Draw.MolToImageFile(molecula, filename="molecula22.jpg",
