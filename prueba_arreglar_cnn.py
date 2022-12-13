973b4dccde31a5979653f3793bc6ba7689a4120d
rvg22 <rvg5423@outlook.com>
2022-12-14 00:06:16 +0100

    mas moleculas y mas modificaciones

--- prueba_arreglar_cnn.py
+++ prueba_arreglar_cnn.py
@@ -1,29 +1,7 @@
-import pandas as pd
+secuencia = '''
+CC[C@@H](C)N(CCC)N(C)C(=O)NC1CCCCCCCC1
+CC[C@@H](C)[C@@H](C)[NH+](C)Cc1ccccc1Cl
+COc1ccccc1CNC(=O)C(=O)NCCCC(=O)NCCC(=O)NCCC(=O)NCCC(=O'''
 
-import csv
-
-with open ("inh.tsv","r") as file:
-    df=pd.read_csv(file, sep="\t", on_bad_lines="skip",low_memory=False)
-    columna_cadena=df["BindingDB Target Chain  Sequence"]
-    columna_50=df["IC50 (nM)"]
-    columna_smiles=df["Ligand SMILES"]
-
-    cadena=[]
-    for i in columna_cadena:
-        cadena.append(i)
-    ic50=[]
-    for i in columna_50:
-        ic50.append(i)
-    smiles=[]
-    for i in columna_smiles:
-        smiles.append(i)
-    headers=["smiles", "IC50","sequence"]
-    listas=[smiles, ic50, cadena]
-    
-with open("probando.csv","w") as archivo:
-    write=csv.writer(archivo)
-    write.writerow(headers)
-    write.writerows(zip(*listas))
-
-        
-
+secuencia = secuencia.replace("\n", "")
+print(secuencia)
