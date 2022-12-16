d9f17103c064c02fa624e182f71d53f16e82e65f
rvg22 <rvg5423@outlook.com>
2022-12-16 22:20:49 +0100

    .

--- pretrained_rnn.py
+++ pretrained_rnn.py
@@ -25,7 +25,7 @@ def split_input_target(chunk):
     return input_text, target
 
 
-with open(r"C:\Users\ASUS\Desktop\github22\dasdsd\smiles_chaval.txt") as f:
+with open(r"C:\Users\ASUS\Desktop\github22\dasdsd\prueba/xaa.txt") as f:
     dades = "\n".join(line.strip() for line in f)
 
 
@@ -91,7 +91,7 @@ for i in range(0, len(dades) - seq_length, 1):
 pattern = dataX[np.random.randint(0, len(dataX)-1)]
 print("\"", ''.join([int_a_elements[value] for value in pattern]), "\"")
 final = ""
-for i in range(50):
+for i in range(100):
     for i in range(random.randrange(50, 137)):
         x = np.reshape(pattern, (1, len(pattern), 1))
         prediction = modelo.predict(x, verbose=0)
