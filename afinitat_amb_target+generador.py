e16c33048de44d605e7c31172654703e752f2276
rvg22 <rvg5423@outlook.com>
2022-12-25 03:22:01 +0100

    afegit score de les molecules generades

--- afinitat_amb_target+generador.py
+++ afinitat_amb_target+generador.py
@@ -1,28 +1,30 @@
+
+from mega import Mega
+import csv
+from keras.layers import Dense, Conv1D, GlobalMaxPooling1D, Embedding, Input, PReLU, Dropout, concatenate, BatchNormalization
+import time
+import random
+from pretrained_rnn import generador
+from check_affinitat import mesurador_afinitat
+import tensorflow as tf
+import pandas as pd
+from keras.models import load_model
+from keras.layers import CuDNNLSTM
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.layers import BatchNormalization
+from keras.callbacks import ModelCheckpoint
+import tensorflow_datasets as tfds
+from rdkit import Chem
 import os
 import sys
 import numpy as np
 from rdkit.Chem import Descriptors, Lipinski
-from rdkit import Chem
-import tensorflow_datasets as tfds
-from keras.callbacks import ModelCheckpoint
-from keras.layers import BatchNormalization
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import CuDNNLSTM
-from keras.models import load_model
-import pandas as pd
-import tensorflow as tf
-from check_affinitat import mesurador_afinitat
-from pretrained_rnn import generador
-import random
-import time
-from keras.layers import Dense, Conv1D, GlobalMaxPooling1D, Embedding, Input, PReLU, Dropout, concatenate, BatchNormalization
-import csv
-from mega import Mega
-import sascorer
+
+# Errors sacore
 from rdkit.Chem import RDConfig
 sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
-
+import sascorer
 
 target = "MAQTQGTKRKVCYYYDGDVGNYYYGQGHPMKPHRIRMTHNLLLNYGLYRKMEIYRPHKANAEEMTKYHSDDYIKFLRSIRPDNMSEYSKQMQRFNVGEDCPVFDGLFEFCQLSTGGSVASAVKLNKQQTDIAVNWAGGLHHAKKSEASGFCYVNDIVLAILELLKYHQRVLYIDIDIHHGDGVEEAFYTTDRVMTVSFHKYGEYFPGTGDLRDIGAGKGKYYAVNYPLRDGIDDESYEAIFKPVMSKVMEMFQPSAVVLQCGSDSLSGDRLGCFNLTIKGHAKCVEFVKSFNLPMLMLGGGGYTIRNVARCWTYETAVALDTEIPNELPYNDYFEYFGPDFKLHISPSNMTNQNTNEYLEKIKQRLFENLRMLPHAPGVQMQAIPEDAIPEESGDEDEEDPDKRISICSSDKRIACEEEFSDSDEEGEGGRKNSSNFKKAKRVKTEDEKEKDPEEKKEVTEEEKTKEEKPEAKGVKEEVKLA"
 
@@ -40,12 +42,15 @@ def buscar_candidats(target=target, forma_guardat="csv", nom_arx="prueba_tio", p
     mirar = []
     score = []
     crear_arxiu(nom_arxiu=nom_arx)
-    while not ic50_menor < 100:
-        generats = generador(nombre_generats=10, img_druglike=False)
-        smiles.extend(generats)
+
+    while not int(ic50_menor) < 100:
+        generats = generador(nombre_generats=2, img_druglike=False)
+        smiles.append(generats)
         for i in generats:
-            score_molecula = sascorer.calculateScore(i)
-            score.append(score_molecula)
+            molecula = Chem.MolFromSmiles(i)
+            print(molecula)
+            sascore = sascorer.calculateScore(molecula)
+            score.append(sascore)
             i = i.replace("@", "").replace("/", "")
             try:
                 predicció_ic50 = mesurador_afinitat(smile=i, fasta=target)
@@ -53,8 +58,8 @@ def buscar_candidats(target=target, forma_guardat="csv", nom_arx="prueba_tio", p
                 mirar.append(predicció_ic50)
             except:
                 ic50.append("error")
-        ic50_menor = int(min(mirar))
-
+        ic50_menor = int(min(ic50))
+        print(score)
         combinacio = list(zip(smiles, ic50, score))
         linies = open(f"{nom_arx}.csv", "r").read()
         with open(f"{nom_arx}.csv", "a", newline="") as file:
