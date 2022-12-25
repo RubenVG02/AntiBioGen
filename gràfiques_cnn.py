5257ae94ffe30a3752133f339346e26685cbdbb2
rvg22 <rvg5423@outlook.com>
2022-12-25 03:22:17 +0100

    modificacions

--- gràfiques_cnn.py
+++ gràfiques_cnn.py
@@ -11,13 +11,13 @@ def crear_gràfiques(path_dades_bones=r"C:\Users\ASUS\Desktop\github22\dasdsd\CS
     dades_reals = pd.read_csv(
         r"C:\Users\ASUS\Desktop\github22\dasdsd\CSV\500k_dades.csv")
 
-    smiles = [smile for smile in dades_reals["smiles"]]
-    fasta = [fasta for fasta in dades_reals["sequence"]]
-    ic50 = [ic50 for ic50 in dades_reals["IC50"]]
-    predits = []
+    smiles = np.array([smile for smile in dades_reals["smiles"]])
+    fasta = np.array([fasta for fasta in dades_reals["sequence"]], dtype="S")
+    ic50 = np.array([ic50 for ic50 in dades_reals["IC50"]], dtype="f")
+    predits = np.array(dtype="f")
     for i in range(50):
         predicció = mesurador_afinitat(smile=smiles[i], fasta=fasta[i])
-        predits.append(predicció)
+        np.append(predits, predicció)
 
     plt.scatter(predits, ic50[0:50])
     plt.xlabel("Valors Predits")
