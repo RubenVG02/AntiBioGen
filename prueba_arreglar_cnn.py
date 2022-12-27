efcf2ec96340ef12ad7de7bb38d9019595a98d59
rvg22 <rvg5423@outlook.com>
2022-12-27 01:05:53 +0100

    modif

--- prueba_arreglar_cnn.py
+++
@@ -1,5 +0,0 @@
-secuencia = '''
-NCCC(=O)NCCC(=O)NCCC(=O)NCCC(=O)NCCC(=O)NCCC(=O)N1CCCCC1CC[C@H](C)Nc1ccc(COc2ccc(C)c(F)c2F)c1COc1ccc(NC(=O)[C@H](O)[C@H](C)Oc2ccc(OC'''
-
-secuencia = secuencia.replace("\n", "")
-print(secuencia)
--- tornar_entrenament.py
+++ tornar_entrenament.py
@@ -10,7 +10,7 @@ from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 
 
-dades = open(r"C:\Users\ASUS\Desktop\github22\dasdsd\prueba\xab.txt").read()
+dades = open(r"C:\Users\ASUS\Desktop\github22\smalls_prueba.txt").read()
 
 # per obtenir els elements unics de dades a numeros enters mitjançant un diccionari
 # així associem un valor numeric a cada lletra
@@ -21,8 +21,8 @@ elements_smiles.update({-1: "\n"})
 int_a_elements = {i: u for i, u in enumerate(sorted(set(dades)))}
 int_a_elements.update({"\n": -1})
 
-mapa_int = len(elements_smiles)
-mapa_char = len(int_a_elements)
+mapa_int = len(elements_smiles)+3
+mapa_char = len(int_a_elements)+3
 
 
 def split_input_target(chunk, valors=mapa_char-1):
@@ -44,7 +44,7 @@ sequences = char_dataset.batch(137+1, drop_remainder=True)
 
 dataset = sequences.map(split_input_target)
 
-dataset = dataset.shuffle(10000).batch(128, drop_remainder=True)
+dataset = dataset.shuffle(20000).batch(128, drop_remainder=True)
 
 modelo = tf.keras.models.Sequential([CuDNNLSTM(128, input_shape=(137, 1), return_sequences=True),
                                      Dropout(0.15),
@@ -63,16 +63,16 @@ modelo = tf.keras.models.Sequential([CuDNNLSTM(128, input_shape=(137, 1), return
 
 
 modelo.load_weights(
-    r"C:\Users\ASUS\Desktop\github22\dasdsd\modelo_rnn_pruebas_bases.hdf5")
+    r"C:\Users\ASUS\Desktop\github22\modelo_rnn_smalls.hdf5")
 
 modelo.compile(optimizer="adam",
                loss="categorical_crossentropy", metrics=["accuracy"])
 
-filepath = "modelo_rnn_pruebas_bases.hdf5"
+filepath = "modelo_rnn_smalls.hdf5"
 checkpoint = ModelCheckpoint(filepath=filepath,
                              monitor='loss',
                              verbose=1,
                              save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-r = modelo.fit(dataset, epochs=100, callbacks=callbacks_list, batch_size=128)
+r = modelo.fit(dataset, epochs=150, callbacks=callbacks_list, batch_size=128)
