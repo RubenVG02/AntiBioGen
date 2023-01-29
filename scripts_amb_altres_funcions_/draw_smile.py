8287b2328b66a1c815b4f6ef9ff84c4d88a8c3bc
RubenVG02 <rubenvillegas2@gmail.com>
2023-01-29 01:06:55 +0100

    aa

--- scripts_amb_altres_funcions_/draw_smile.py
+++ scripts_amb_altres_funcions_/draw_smile.py
@@ -6,8 +6,8 @@ smile = "OC(=O)[C@@H](C)[NH2+][C@@H](Cc1c[nH]c2ccccc12)C(=O)NCC(C)C"
 
 FILENAME=""
 
-molecula = Chem.MolFromSmiles(smile)
-Draw.MolToImageFile(molecula, filename=FILENAME,
+molecule = Chem.MolFromSmiles(smile)
+Draw.MolToImageFile(molecule, filename=FILENAME,
                     size=(400, 300))
 
 
